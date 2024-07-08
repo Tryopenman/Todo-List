d1fd390e997bb07dd35e4ae0a72489c37475d862
Francisco Osório <franciscoosorio2004@gmail.com>
2024-07-08 19:28:34 +0100

    feat: register-task

---
+++ src/main/java/com/franciscoosorio/todolist/task/TaskController.java
@@ -0,0 +1,43 @@
+package com.franciscoosorio.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity registerTask(@RequestBody TaskModel task, HttpServletRequest request){
+
+        UUID idUser = (UUID) request.getAttribute("idUser");
+        task.setIdUser(idUser);
+
+        var current_date = LocalDateTime.now();
+
+        if (current_date.isAfter(task.getStartAt()) || current_date.isAfter(task.getEndAt()))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start date or end date must be greater than the current date!");
+
+        if (task.getStartAt().isAfter(task.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The end date must be greater than the start date!");
+        }
+            
+        
+        var taskRegistered = this.taskRepository.save(task);
+
+        return ResponseEntity.status(HttpStatus.OK).body(taskRegistered);
+    }
+}

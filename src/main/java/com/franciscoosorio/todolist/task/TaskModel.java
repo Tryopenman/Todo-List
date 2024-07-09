b3816c4f51a7fcec8266ec6d4f1ec061a503a595
Francisco Osório <franciscoosorio2004@gmail.com>
2024-07-09 11:56:32 +0100

    bug:fix title length exception

--- src/main/java/com/franciscoosorio/todolist/task/TaskModel.java
+++ src/main/java/com/franciscoosorio/todolist/task/TaskModel.java
@@ -29,4 +29,12 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception{
+
+        if (title.length() > 80) {
+            throw new Exception("The title field must only contain a maximum of 50 characters!");
+        }
+        this.title = title;
+    }
 }

8589a5c1e288ff379522467c4822a33398ff42cf
Francisco Osório <franciscoosorio2004@gmail.com>
2024-07-09 11:32:39 +0100

    feat:update-task

--- src/main/java/com/franciscoosorio/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/franciscoosorio/todolist/filter/FilterTaskAuth.java
@@ -28,7 +28,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         
         var servletPath = request.getServletPath();
 
-        if (servletPath.equals("/tasks/")) {
+        if (servletPath.startsWith("/tasks/")) {
             String authorization = request.getHeader("Authorization");
 
             String authEncoded = authorization.substring("Basic".length()).trim();
--- src/main/java/com/franciscoosorio/todolist/task/TaskController.java
+++ src/main/java/com/franciscoosorio/todolist/task/TaskController.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import java.util.List;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.franciscoosorio.todolist.utils.Utils;
+
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -48,5 +55,27 @@ public class TaskController {
         
         var idUser = request.getAttribute("idUser");
         return this.taskRepository.findByIdUser((UUID) idUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateTask(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.badRequest().body("Task not found!");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.badRequest().body("User does not have permissions!");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        var task_updated = this.taskRepository.save(task);
+
+        return ResponseEntity.ok().body(task_updated);
     }
 }
